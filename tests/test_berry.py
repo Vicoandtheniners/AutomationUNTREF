01d87d863ae6523e4f62821f2e98aac0a72c1b6b
Vicoandtheniners <vicotero.fran@gmail.com>
2024-10-23 05:00:17 -0300

    Agregando nuevo caso de prueba Test Berry

--- tests/test_berry.py
+++ tests/test_berry.py
@@ -5,9 +5,18 @@ class TestBerryAPI(unittest.TestCase):
     def test_berry_1(self):
         url = "https://pokeapi.co/api/v2/berry/1"
         response = requests.get(url)
-        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.status_code, 200, f"Error: {response.status_code}. No se pudo acceder al endpoint.")
+
         berry_data = response.json()
-        self.assertEqual(berry_data['size'], 20)
+
+        self.assertEqual(berry_data['size'], 20, f"El tamaño esperado es 20, pero se recibió {berry_data['size']}.")
+
+        self.assertEqual(berry_data['soil_dryness'], 15, f"La sequedad del suelo esperada es 15, pero se recibió {berry_data['soil_dryness']}.")
+
+        self.assertEqual(berry_data['firmness']['name'], "soft", f"Firmeza esperado es 'soft', pero se recibió '{berry_data['firmness']['name']}'.")
+
+        print('La prueba ha finalizado')
 
 if __name__ == '__main__':
     unittest.main()
